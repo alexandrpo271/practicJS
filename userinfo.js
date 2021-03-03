dfcc45ca6dd8a9780f178b651a2956099c470a34
Alexandr Pokrishko <alexandrpo271@gmail.com>
2021-03-04 01:07:43 +0300

    first commit

---
+++ userinfo.js
@@ -0,0 +1,27 @@
+const age = prompt ('Укажите ваш возаст');
+const gender = confirm ('Если ваш пол мужской, нажмите "ОК"');
+
+if ( age > 60 && gender === true ) {
+    console.log('Вы дедушка')
+}
+else if ( age > 19 && gender === true ) {
+    console.log('Вы мужчина')
+}
+else if ( age > 11 && gender === true ) {
+    console.log('Вы юноша')
+}
+else if ( age <=11 && gender === true ) {
+    console.log('Вы мальчик')
+}
+if ( age > 60 && gender === false ) {
+    console.log('Вы бабушка')
+}
+else if ( age > 19 && gender === false ) {
+    console.log('Вы женщина')
+}
+else if ( age > 11 && gender === false ) {
+    console.log('Вы девушка')
+}
+else if ( age <=11 && gender === false ) {
+    console.log('Вы девочка')
+}
